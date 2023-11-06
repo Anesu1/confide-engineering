bf91cd0f45f30890936a4fcc8fe33a5a7ff6141c
Anesu1 <ndoroansue01@gmail.com>
2023-11-06 17:58:44 +0200

    update

--- app/about/missionandvalues.jsx
+++ app/about/missionandvalues.jsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import React from "react";
+import { productSansBold } from "../font/font";
 
 export default function MissionAndValues() {
   return (
     <div className="md:flex md:gap-1 mb-1">
                     <Image height={100} width={100} className="relative w-[13%] max-w-[165px] hidden object-contain lg:block " src="/images/about/mission1.webp " alt="" /> 
       <div className="bg-[#00ADF1] text-white m-2 p-5 md:w-[40%] lg:m-0 max-w-[451px]" >
-        <h3 className="text-2xl mb-5">Mission Statement</h3>
+        <h3 className={`${productSansBold.className} text-2xl mb-5`}>Mission Statement</h3>
         <p className='text-md'>
           Our mission is to identify contemporary market needs, innovate and
           deliver quality engineering solutions for the local and the global
@@ -17,7 +18,7 @@ export default function MissionAndValues() {
       <Image height={100} width={100} className="relative object-contain w-[29%] max-w-[290px] hidden lg:block" src="/images/about/mission3.webp" alt="" />
       
       <div className="bg-[#00ADF1] text-white m-2 p-5 md:w-[30%] max-w-[223px] lg:m-0">
-        <h3 className="text-2xl mb-5">Vision Statement</h3>
+        <h3 className={`${productSansBold.className} text-2xl mb-5`}>Vision Statement</h3>
         <p className='text-md '>
           To make the world a better place by providing innovative products and
           utilities that enhances life
