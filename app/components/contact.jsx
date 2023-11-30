9052c8c9df59b6c3e7368381d46bec66882569af
Anesu1 <ndoroansue01@gmail.com>
2023-11-30 15:36:58 +0200

    updated contact section

--- app/components/contact.jsx
+++ app/components/contact.jsx
@@ -5,12 +5,13 @@ import { productSansBold } from "../font/font";
 
 
 export default function Contact() {
+  
   return (
-    <section id="contact" class="p-5 md:p-[5%]">
+    <section id="contact" className="p-5 md:p-[5%]">
       <h3 className={` ${productSansBold.className} text-3xl pb-2`}>Have a project in mind?</h3>
       <p className="font-light">
         For inquiries or to discuss your container needs, please reach out to
-        our dedicated team at Containawise. We specialize in innovative
+        our dedicated team at Confide Engineering. We specialize in innovative
         solutions for offices, accommodations, ablution facilities, storage, and
         tuckshops. Let&apos;s collaborate to bring your vision to life. Contact us
         today!
--- app/components/styled/form.jsx
+++ app/components/styled/form.jsx
@@ -1,34 +1,161 @@
 "use client"
 import React from 'react'
+import {useState} from "react"
 import TextField from '@mui/material/TextField';
 import { avenirNext, productSansBold } from '@/app/font/font';
+import { Fade } from 'react-reveal';
+import axios from "axios";
 
 
 export default function Form() {
+   const [isSubmitted, setIsSubmitted] = useState(false);
+   const [isFailed, setIsFailed] = useState("");
+   const [formData, setFormData] = useState({
+     fullName: "",
+     email: "",
+     contactNumber:"",
+     subject:"",
+     message: "",
+   });
+
+   const handleInputChange = (event) => {
+     const { name, value } = event.target;
+     setFormData({ ...formData, [name]: value });
+   };
+
+   const handleSubmit = async (event) => {
+     event.preventDefault();
+     await axios.post(
+        "https://formsubmit.co/a64be50d7a64fbe989df721fe6359a9e",
+        formData
+      ).then((res) => {
+        console.log(res)
+         console.log("Form submission successful:", response.data);
+         setIsSubmitted(true);
+         setTimeout(() => {
+           setIsSubmitted(false);
+           setFormData({
+             fullName: "",
+             email: "",
+             contactNumber: "",
+             subject: "",
+             message: "",
+           });
+         }, 5000);
+      }).catch((error) => {
+        console.log(error)
+        setIsFailed("Form submission failed");
+        setTimeout(() => {
+          setIsFailed("");
+        }, 5000);
+      })
+
+
+
+    //  try {
+    //    // Send form data to server or API endpoint
+    //    const response = await axios.post(
+    //      "https://formsubmit.co/a64be50d7a64fbe989df721fe6359a9e",
+    //      formData
+    //    );
+    //    console.log("Form submission successful:", response.data);
+    //    setIsSubmitted(true);
+    //    setTimeout(() => {
+    //      setIsSubmitted(false);
+    //      setFormData({
+    //        fullName: "",
+    //        email: "",
+    //        contactNumber: "",
+    //        subject: "",
+    //        message:""
+    //      });
+    //    }, 5000);
+    //  } catch (error) {
+    //   console.log(error)
+    //    setIsFailed("Form submission failed");
+    //    setTimeout(() => {
+    //      setIsFailed("");
+    //    }, 5000);
+    //  }
+   };
   return (
-    <form action="" className={`${ avenirNext.className } mt-5 md:flex md:flex-wrap gap-3 justify-between`}>
-      
+    <>
+      <form
+        method="POST"
+        onSubmit={handleSubmit}
+        className={`${avenirNext.className} mt-5 md:flex md:flex-wrap gap-3 justify-between`}
+      >
         <div className="uppercase md:w-[49%] lg:w-[40.5%]">
-                    <TextField className='w-full  bg-[#F2F2F2] mb-5' id="username" label="Name" variant="standard" />
-                    <TextField className='w-full  bg-[#F2F2F2] mb-5' id="email" type="email" label="Example@mail.com" variant="standard" />
-                    <TextField className='w-full  bg-[#F2F2F2] mb-5' id="contact-number" label="contact number" variant="standard" />
-                    
-                </div>
-                <div className="uppercase md:w-[49%] lg:w-[58%]">
-                <TextField className='w-full  bg-[#F2F2F2] mb-5' id="filled-basicw" label="Name" variant="standard" />
-                <TextField
-                  id="filled-multiline-static"
-                  className='w-full bg-[#F2F2F2] mb-5'
-                  label="Message"
-                  multiline
-                  rows={7}
-                  variant="filled"
-                />
-                </div>
-     
-        
-        <button className='bg-[#005180] text-white h-[50px] text-center w-full'>Send Message</button>
-    </form>
-  )
+          <TextField
+            className="w-full  bg-[#F2F2F2] mb-5"
+            id="username"
+            label="Name"
+            variant="standard"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleInputChange}
+          />
+          <TextField
+            className="w-full  bg-[#F2F2F2] mb-5"
+            id="email"
+            type="email"
+            name="email"
+            label="Example@mail.com"
+            value={formData.email}
+            onChange={handleInputChange}
+            variant="standard"
+          />
+          <TextField
+            className="w-full  bg-[#F2F2F2] mb-5"
+            id="contact-number"
+            name="contactNumber"
+            label="contact number"
+            value={formData.contactNumber}
+            onChange={handleInputChange}
+            variant="standard"
+          />
+        </div>
+        <div className="uppercase md:w-[49%] lg:w-[58%]">
+          <TextField
+            className="w-full  bg-[#F2F2F2] mb-5"
+            id="filled-basicw"
+            label="Subject"
+            name="subject"
+            value={formData.subject}
+            onChange={handleInputChange}
+            variant="standard"
+          />
+          <TextField
+            id="filled-multiline-static"
+            className="w-full bg-[#F2F2F2] mb-5"
+            label="Message"
+            multiline
+            name="message"
+            value={formData.message}
+            onChange={handleInputChange}
+            rows={7}
+            variant="filled"
+          />
+        </div>
+
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className="bg-[#005180] text-white h-[50px] text-center w-full"
+        >
+          Send Message
+        </button>
+        {isSubmitted && (
+        <div style={{ color: "green", marginTop: 20, textAlign: "center" }}>
+          <Fade bottom>Submitted successfully!</Fade>{" "}
+        </div>
+      )}
+      <div style={{ color: "red", marginTop: 20, textAlign: "center" }}>
+        <Fade bottom>{isFailed}</Fade>
+      </div>
+      </form>
+      
+    </>
+  );
 }
 
