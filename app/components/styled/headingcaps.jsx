81e9ccac45de621bfa52ede0db570cbdc952fa97
Anesu1 <anesu@uncommon.org>
2025-01-28 15:56:04 +0200

    refactor: remove unused font import from HeadingCaps component

--- app/components/styled/headingcaps.jsx
+++ app/components/styled/headingcaps.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import { productSansBold } from '@/app/font/font'
 
 export default function HeadingCaps({first, middle, last}) {
   return (
     <div className='mb-5'>
       
-          <h2 className={`${productSansBold.className}  text-[57px] lg:text-[100px] xl:text-[130px]`}>{first}</h2>
-          <h5 className={`${ productSansBold.className } text-2xl md:text-4xl `}>{middle}</h5>
-          <h2 className={`${productSansBold.className} text-[57px] lg:text-[100px] xl:text-[130px]`}>{last}</h2>
+          <h2 className={`  text-[57px] lg:text-[100px] xl:text-[130px]`}>{first}</h2>
+          <h5 className={` text-2xl md:text-4xl `}>{middle}</h5>
+          <h2 className={` text-[57px] lg:text-[100px] xl:text-[130px]`}>{last}</h2>
     </div>
   )
 }
